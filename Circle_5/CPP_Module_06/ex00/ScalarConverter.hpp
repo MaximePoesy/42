e772c32a2ecde201e786b3ae20f776e4de98bae2
Maxime Poesy <mpoesy@student.42luxembourg.lu>
2025-12-17 15:45:31 +0100

    start ex00

---
+++ Circle_5/CPP_Module_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+# include <string>
+
+class ScalarConverter
+{
+	private:
+		ScalarConverter();
+	public:
+		~ScalarCOnverter();
+		static void convert(std::string str);
+};
+
+# endif
