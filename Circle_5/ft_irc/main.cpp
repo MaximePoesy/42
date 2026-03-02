534fb5e4ee542bbdad4b9947d2aa932a8ca81a5c
Maxime Poesy <mpoesy@student.42luxembourg.lu>
2026-03-02 17:57:07 +0100

    00

--- Circle_5/ft_irc/main.cpp
+++ Circle_5/ft_irc/main.cpp
@@ -8,39 +8,36 @@ int	main(int argc, char **argv)
 	addr.sin_family = AF_INET; // IPv4
 	addr.sin_port = htons(port); // converts hostshort to network byte order (switch endians)
 	addr.sin_addr.s_addr = INADDR_ANY; // listen to any address
-
-	memset(&addr, 0, sizeof(addr));
-	bind(server_fd, (sockaddr*)&addr, sizeof(addr));
+	memset(&addr, 0, sizeof(addr)); // zero out padding bytes to avoid garbage
+	bind(server_fd, (sockaddr*)&addr, sizeof(addr)); // bind socket to address/port
 	
-	listen(server_fd, SOMAXCONN);
+	listen(server_fd, SOMAXCONN); // mark socket as passive, SOMAXCONN = max pending connections
 	
-	std::vector<pollfd> fds;
+	std::vector<pollfd> fds; // poll watches multiple fds at once
 	pollfd server_poll;
 	server_poll.fd = server_fd;
-	server_poll.events = POLLIN;
-	server_poll.revents = 0;
-
+	server_poll.events = POLLIN; // notify when data is ready to read
+	server_poll.revents = 0; // kernel writes here what actually happened
 	fds.push_back(server_poll);
 	
 	while (true)
 	{
-		int ret = poll(&fds[0], fds.size(), -1);
+		int ret = poll(&fds[0], fds.size(), -1); // blocks until any fd is ready, -1 = no timeout
 		if (ret < 0)
-		continue; // handle error properly
+			continue; // TODO : handle error properly
 		
 		for (size_t i = 0; i < fds.size(); ++i)
 		{
-			if (fds[i].revents & POLLIN)
+			if (fds[i].revents & POLLIN) // fd has incoming data
 			{
 				if (fds[i].fd == server_fd)
-					handleNewConnection();
+					handleNewConnection(); // TODO : new client is knocking -- call accept()
 				else
-					handleClientRead(fds[i].fd);
+					handleClientRead(fds[i].fd); // TODO : existing client sent data
 			}
-
-			if (fds[i].revents & POLLOUT)
+			if (fds[i].revents & POLLOUT) // fd is ready to send data without blocking
 			{
-				handleClientWrite(fds[i].fd);
+				handleClientWrite(fds[i].fd); //TODO
 			}
 		}
 	}
