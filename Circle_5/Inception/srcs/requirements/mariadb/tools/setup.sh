f184dea7e88d8751c273978178beaacdc0010302
mpoesy <mpoesy@student.42luxembourg.lu>
2026-05-26 11:05:01 +0200

    Inception finished

--- Circle_5/Inception/srcs/requirements/mariadb/tools/setup.sh
+++ Circle_5/Inception/srcs/requirements/mariadb/tools/setup.sh
@@ -2,15 +2,17 @@
 #
 echo "Starting database config..."
 
-mysqld_safe & 
-until mysqladmin ping \
-	-u root \
-	--silent; do
-	echo "waiting for MariaDB..."
-	sleep 2
-done
-echo "Starting..."
-env -u MYSQL_HOST mysql -u root -h localhost << EOF
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+	mysqld_safe & 
+	until mysqladmin ping \
+		-u root \
+		-p"${MYSQL_ROOT_PASSWORD}" \
+		--silent; do
+		echo "waiting for MariaDB..."
+		sleep 2
+	done
+	echo "Starting..."
+	env -u MYSQL_HOST mysql -u root -h localhost << EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
@@ -19,7 +21,9 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'
 FLUSH PRIVILEGES;
 EOF
 
-echo "Database setup finished!"
-#service mariadb stop
+	echo "Database setup finished!"
+	#service mariadb stop
+	mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+fi
 
-wait
+exec mysqld_safe
